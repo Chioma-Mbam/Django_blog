d902ff69b89d69d1967f1f7476fb8e29c398f409
Chioma-Mbam <mbamrosemary425@gmail.com>
2026-06-30 14:55:14 +0100

    blog app and model

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blogs',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +131,7 @@ STATIC_ROOT = BASE_DIR /"static"
 STATICFILES_DIRS = [
     "blog_main/static",
 ]
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
