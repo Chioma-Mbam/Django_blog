d0ef96530d8a0d1b8b1abae34ec0c568cae17fcd
Chioma-Mbam <mbamrosemary425@gmail.com>
2026-07-13 14:05:39 +0100

    context_processors added

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
+from blogs.models import Category, Blog
+
+# Create your views here.
 
 def home(request):
-    return render(request, "home.html")
+    featured_posts = Blog.objects.filter(is_featured=True, status="Published").order_by("updated_at")
+    recent_posts = Blog.objects.filter(is_featured=False, status="Published").order_by("-created_at")
+    context = {
+        "featured_posts":featured_posts,
+        "recent_posts":recent_posts,
+    }
+    return render(request, "home.html", context)
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from.models import Blog, Category
 
-# Create your views here.
+
+def posts_by_category(request, category_id):
+    # Fetch the category with the id category_id
+    post = Blog.objects.filter(status='Published', category=category_id)
+    
+    # # Use try/except when you want to do some custom action if category does not exist
+    try:
+        category = Category.objects.get(pk=category_id)
+    except:
+        return redirect('home')
+    
+    # # Use get_object_404 when you want to show 404 error when category does not exist.
+    # category = get_object_or_404(Category, pk=category_id)
+    
+    context={
+        'post':post,
+        'category':category,
+    }
+    return render(request, 'posts_by_category.html', context)
